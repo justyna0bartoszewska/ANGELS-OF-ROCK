c7270cf7e351ae5cd75c9fa921631978bf1a7a76
DESKTOP-M865K73\Uzytkownik <justyna.bartoszewska.jb@gmail.com>
2018-05-12 11:35:55 +0200

    anielski gang wersja 1

---
+++ gang.php
@@ -0,0 +1,21 @@
+<?php
+/** Template Name: Anielski Gang
+ *
+ * This is the template that displays full width page without sidebar
+ *
+ * @package activello
+ */
+
+get_header(); ?>
+
+  <div id="primary" class="content-area">
+
+	<main id="main" class="site-main" role="main">
+
+		
+
+	</main><!-- #main -->
+
+  </div><!-- #primary -->
+
+<?php get_footer(); ?>
